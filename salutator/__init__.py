018fb01f6320368f2da7eb81c47d353f9bd460a9
Juan M. Carmona Loaiza <juanmcloaiza@gmail.com>
2025-10-23 11:27:32 +0200

    Expose submodules at top-level __init__.py

--- salutator/__init__.py
+++ salutator/__init__.py
@@ -4,8 +4,18 @@ Salutator — a package for greetings and goodbyes
 across humans, animals, plants, and minerals.
 """
 
+from . import humans
+from . import animals
+from . import plants
+from . import minerals
+
+
 #  Version info (__version__)
-#from ._version import version as __version__
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0"
+
 
 __all__ = ['humans', 'animals', 'plants', 'minerals']
 
